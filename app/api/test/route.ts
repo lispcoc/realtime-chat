df3e5195dfba36db7a0d0c7770ad12044ecdbe5b
LISPCoC <fourier0520@gmail.com>
2026-01-11 18:48:55 +0900

    update

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -3,5 +3,5 @@ import { supabase } from "@/utils/supabase/supabase"
 
 export async function GET() {
     const { data } = await supabase.from("Rooms").select("*")
-    return Response.json({ rooms: data })
+    return { rooms: data }
 }
