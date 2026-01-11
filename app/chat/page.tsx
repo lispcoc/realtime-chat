042180e98afff0c4e7cb3d435d085f42bf8fa869
LISPCoC <fourier0520@gmail.com>
2026-01-12 00:44:20 +0900

    update

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -207,7 +207,7 @@ export default function Chat() {
   }
 
   return (
-    <div className="flex-1 w-full flex flex-col p-2">
+    <div className="">
       <h2 className="text-3xl font-bold pt-5 pb-10">{roomData ? roomData.title : ""}</h2>
 
       {!isEntered && (
@@ -226,7 +226,7 @@ export default function Chat() {
 
       {isEntered && (
         <form className="w-full max-w-md pb-10" onSubmit={onSubmitLeave}>
-          <button type="submit" disabled={inputName === ""} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center disabled:opacity-25">
+          <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center disabled:opacity-25">
             退室
           </button>
         </form>
@@ -236,7 +236,8 @@ export default function Chat() {
 
         {isEntered && (
           <div className="mb-1">
-            <label style={{ whiteSpace: 'nowrap' }} htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">{username}</label>
+            <label style={{ whiteSpace: 'nowrap' }} htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900"></label>
+            <span>{username}</span>
             <input style={{ whiteSpace: 'nowrap' }} type="text" id="message" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
                 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               name="message" value={inputText} onChange={(event) => setInputText(() => event.target.value)}></input>
