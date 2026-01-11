abdd21cd0f56fdc0f3a712052d23844290795bbb
LISPCoC <fourier0520@gmail.com>
2026-01-11 18:43:54 +0900

    update

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,17 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { supabase } from "@/utils/supabase/supabase"
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
-    if (req.method === "POST") {
-        const { value } = req.body;
-        return res.status(200).json({ test: "ok" });
-    }
-    if (req.method === "GET") {
-        const { value } = req.body;
-        const { data } = await supabase.from("Rooms").select("*")
-        return res.status(200).json({ rooms: data });
-    }
+export async function GET() {
+    const { data } = await supabase.from("Rooms").select("*")
+    return Response.json({ rooms: data })
 }
