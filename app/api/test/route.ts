8579806bfeed15c8b7469857a8aff5a2740e18bf
LISPCoC <fourier0520@gmail.com>
2026-01-11 18:57:20 +0900

    update

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,6 +2,5 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { supabase } from "@/utils/supabase/supabase"
 
 export async function GET() {
-    const { data } = await supabase.from("Rooms").select("*")
-    return { rooms: data }
+    return
 }
