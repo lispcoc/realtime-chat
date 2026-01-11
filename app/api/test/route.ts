4a76c9bac3164b83f1cccbbed79d340b9b854cfc
LISPCoC <fourier0520@gmail.com>
2026-01-11 18:59:07 +0900

    update

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,6 +1,7 @@
+import { NextResponse } from 'next/server'
 import type { NextApiRequest, NextApiResponse } from "next";
 import { supabase } from "@/utils/supabase/supabase"
 
 export async function GET() {
-    return
+    return NextResponse.json({ response: 'ok' })
 }
