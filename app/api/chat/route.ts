c82ff94fd62cc08cec4d924f2923242710460ccc
LISPCoC <fourier0520@gmail.com>
2026-01-11 22:33:32 +0900

    update

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,7 +4,7 @@ import { headers } from "next/headers";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { supabase } from "@/utils/supabase/supabase"
 
-async function addMessage(msg : any) {
+async function addMessage(msg: any) {
     await supabase.from("Messages").insert(msg)
 }
 
@@ -30,7 +30,6 @@ export async function POST(request: NextRequest) {
                 color: 0
             })
             addMessage({
-                created_at: new Date(Date.now()).toISOString(),
                 color: 0,
                 name: "system",
                 room_id: parseInt(roomId),
