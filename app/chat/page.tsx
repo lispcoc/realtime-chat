e9e3fffac56dc7280429e10fac37f387b6cf2456
LISPCoC <fourier0520@gmail.com>
2026-01-12 00:22:16 +0900

    update

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -236,7 +236,7 @@ export default function Chat() {
 
         {isEntered && (
           <div className="mb-5">
-            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">username</label>
+            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">{username}</label>
             <input type="text" id="message" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
                 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               name="message" value={inputText} onChange={(event) => setInputText(() => event.target.value)}></input>
