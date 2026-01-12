75182ae2075e81af3dda8df5719d671f77adc18a
LISPCoC <fourier0520@gmail.com>
2026-01-12 17:38:15 +0900

    update

--- app/editRoom/page.tsx
+++ app/editRoom/page.tsx
@@ -166,7 +166,6 @@ export default function CreateRoom() {
             <input
               type="password" id="roomNewPassword" name="roomNewPassword" placeholder="パスワード"
               value={inputNewPassword} onChange={(event) => setInputNewPassword(() => event.target.value)}
-              required
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             />
           </div>
