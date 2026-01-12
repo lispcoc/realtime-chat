107eaa9d265d7ec6ddf2a9b13d18178c08677b81
lispcoc <fourier0520@gmail.com>
2026-01-13 04:08:17 +0900

    update

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -367,14 +367,17 @@ export default function Chat() {
           <div className="mb-1">
             <label htmlFor="message" className="block inline-block mb-2 font-medium text-gray-900"></label>
             <span style={{ color: color }} className="mb-2 font-medium text-gray-900">{username}</span>
+            <button type="submit" disabled={buttonDisable || inputText === ""}
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4
+              focus:outline-none focus:ring-blue-300 font-medium rounded-lg
+              text-sm w-full sm:w-auto px-5 py-2.5 text-center disabled:opacity-25">
+              発言
+            </button>
             <textarea id="message" name="message" rows={1}
               className="block p-2.5 mb-2 w-full text-sm text-gray-900
                 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
               value={inputText} onChange={(event) => setInputText(() => event.target.value)}
             />
-            <button type="submit" disabled={buttonDisable || inputText === ""} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center disabled:opacity-25">
-              発言
-            </button>
           </div>
         </form>
       )}
