8f6cd5f14df2c03f81fdac8e0325b800052c9f04
LISPCoC <fourier0520@gmail.com>
2026-01-12 14:50:41 +0900

    update

--- app/api/dice/route.ts
+++ app/api/dice/route.ts
@@ -1,18 +1,10 @@
 import { NextResponse } from 'next/server'
 import { headers } from "next/headers";
-import { Base as Dice } from 'bcdice';
 
 export async function GET() {
     const headersList = headers();
     const ip = headersList.get("x-forwarded-for");
 
-    const result = Dice.eval("2d6")
-    if (result && result.text) {
-        return NextResponse.json({
-            response: 'ok',
-            result: result.text
-        })
-    }
     return NextResponse.json({
         response: 'ok',
         ip: ip
