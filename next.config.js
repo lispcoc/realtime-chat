028a79ef6f82e0ed19beb650f2d1e2f9aafbbb40
lispcoc <fourier0520@gmail.com>
2026-01-19 10:01:32 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {
-    rules: {
-      '*.mp3': {
-        loaders: ['raw-loader'],
-        as: '*.js'
+  webpack (config, options) {
+    config.module.rules.push({
+      test: /\.mp3$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/chunks/[path][name].[hash][ext]'
       }
-    }
+    })
+    return config
   }
 }
 
