185aa47b5757362298283b5c28a516b60abb1b95
LISPCoC <fourier0520@gmail.com>
2026-01-27 16:13:24 +0900

    update

--- utils/supabase/supabase.ts
+++ utils/supabase/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 import { Database } from '@/types/supabasetype'
 
 export const supabase = createClient<Database>(
-  process.env.MY_PUBLIC_SUPABASE_URL!,
-  process.env.MY_PUBLIC_SUPABASE_ANON_KEY!,
+  process.env.NEXT_PUBLIC_MY_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_MY_SUPABASE_ANON_KEY!,
 );
