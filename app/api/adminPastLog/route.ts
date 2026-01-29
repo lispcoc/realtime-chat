11a4cb4fe50f8a56528807d45b528a648862b53a
LISPCoC <fourier0520@gmail.com>
2026-01-29 17:30:25 +0900

    update

--- app/api/adminPastLog/route.ts
+++ app/api/adminPastLog/route.ts
@@ -6,64 +6,46 @@ import { google } from 'googleapis'
 import { oauth2Client } from '@/lib/google/oauth'
 import { cookies } from 'next/headers';
 
-export async function GET() {
-  const client = await createClient()
-  const res = await client.auth.getUser()
-  if (res.data) {
-
-    return NextResponse.json(
-      {
-        status: 200,
-      }
-    )
-  }
-
-  return NextResponse.json(
-    { error: "Basic Auth Required" },
-    {
-      headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
-      status: 401,
-    }
-  )
-}
-
 export async function POST(request: NextRequest) {
-  const { roomId, page } = await request.json();
-  const currentSessionToken = (await cookies()).get('session')?.value
-  console.log(currentSessionToken)
-  if (currentSessionToken) {
-    oauth2Client.setCredentials(JSON.parse(currentSessionToken))
-    const oauth2 = google.oauth2({
-      auth: oauth2Client,
-      version: "v2"
-    })
-    const res = await oauth2.userinfo.get()
-    console.log(res.data)
-    if (res && res.data && res.data.email === process.env.ADMIN_EMAIL) {
-      let allMessages: Database["public"]["Tables"]["Messages"]["Row"][] = []
-      try {
-        if (roomId) {
-          const { data } = await supabase.from("Messages").select("*").eq('room_id', roomId).order("created_at", { ascending: false }).range(page * 1000, (page + 1) * 1000)
-          if (data != null) {
-            allMessages = data
+  try {
+    const { roomId, page } = await request.json();
+    const currentSessionToken = (await cookies()).get('session')?.value
+    console.log(currentSessionToken)
+    if (currentSessionToken) {
+      oauth2Client.setCredentials(JSON.parse(currentSessionToken))
+      const oauth2 = google.oauth2({
+        auth: oauth2Client,
+        version: "v2"
+      })
+      const res = await oauth2.userinfo.get()
+      console.log(res.data)
+      if (res && res.data && res.data.email === process.env.ADMIN_EMAIL) {
+        let allMessages: Database["public"]["Tables"]["Messages"]["Row"][] = []
+        try {
+          if (roomId) {
+            const { data } = await supabase.from("Messages").select("*").eq('room_id', roomId).order("created_at", { ascending: false }).range(page * 1000, (page + 1) * 1000)
+            if (data != null) {
+              allMessages = data
+            }
+          } else {
+            const { data } = await supabase.from("Messages").select("*").order("created_at", { ascending: false }).range(page * 1000, (page + 1) * 1000)
+            if (data != null) {
+              allMessages = data
+            }
           }
-        } else {
-          const { data } = await supabase.from("Messages").select("*").order("created_at", { ascending: false }).range(page * 1000, (page + 1) * 1000)
-          if (data != null) {
-            allMessages = data
-          }
+        } catch (error) {
+          console.error(error)
         }
-      } catch (error) {
-        console.error(error)
+        return NextResponse.json(
+          {
+            result: 'ok',
+            messages: allMessages
+          },
+          { status: 200 }
+        )
       }
-      return NextResponse.json(
-        {
-          result: 'ok',
-          messages: allMessages
-        },
-        { status: 200 }
-      )
     }
+  } catch (error) {
   }
 
   return NextResponse.json(
--- app/api/auth/google-oauth/getUserData/route.tsx
+++ app/api/auth/google-oauth/getUserData/route.tsx
@@ -4,29 +4,37 @@ import { NextResponse, NextRequest } from "next/server";
 import { cookies } from 'next/headers';
 
 export async function POST(request: NextRequest) {
-  const { tokens } = await request.json()
+  try {
+    const { tokens } = await request.json()
 
-  if (tokens) {
-    (await cookies()).set('session', JSON.stringify(tokens), {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      maxAge: 60 * 60 * 24 * 7,
-      path: '/'
+    if (tokens) {
+      (await cookies()).set('session', JSON.stringify(tokens), {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 60 * 24 * 7,
+        path: '/'
+      })
+    }
+
+    const currentSessionToken = (await cookies()).get('session')?.value || "{}"
+
+    oauth2Client.setCredentials(tokens || JSON.parse(currentSessionToken))
+    const oauth2 = google.oauth2({
+      auth: oauth2Client,
+      version: "v2"
     })
+
+    const res = await oauth2.userinfo.get()
+
+    return NextResponse.json(
+      { result: 'ok', res: res },
+      { status: 200 }
+    )
+  } catch (error) {
   }
 
-  const currentSessionToken = (await cookies()).get('session')?.value || "{}"
-
-  oauth2Client.setCredentials(tokens || JSON.parse(currentSessionToken))
-  const oauth2 = google.oauth2({
-    auth: oauth2Client,
-    version: "v2"
-  })
-
-  const res = await oauth2.userinfo.get()
-
   return NextResponse.json(
-    { result: 'ok', res: res },
+    { result: 'ng' },
     { status: 200 }
   )
 }
