c1ec1afcc1faee18fe9a999d175af4a8c84cc700
lispcoc <fourier0520@gmail.com>
2026-01-30 09:59:14 +0900

    update

--- app/api/auth/google-oauth/callback/route.tsx
+++ app/api/auth/google-oauth/callback/route.tsx
@@ -1,37 +1,30 @@
-import { createOAuth2Client, setOAuthTokenCookie } from '@/lib/google/oauth'
+import { oauth2Client } from '@/lib/google/oauth'
 import { google } from 'googleapis'
-import { redirect } from "next/navigation";
-import { type NextRequest } from "next/server";
-import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
+import { redirect } from "next/navigation"
+import { type NextRequest } from "next/server"
+import { createClient } from "@/utils/supabase/server"
+import { cookies } from "next/headers"
 
 export async function GET(req: NextRequest) {
-
   const searchParams = req.nextUrl.searchParams;
-  const code = searchParams.get("code");
+  const code = searchParams.get("code")
 
   if (!code) {
     return new Response(`Missing query parameter`, {
       status: 400,
-    });
+    })
   }
 
-  const supabase = await createClient()
-  const { data, error: tokenError } = await supabase.auth.exchangeCodeForSession(code)
+  const { tokens } = await oauth2Client.getToken(code)
 
-  /*
-  const oauth2Client = createOAuth2Client();
-  const { tokens } = await oauth2Client.getToken(code);
-
-  oauth2Client.setCredentials(tokens)
-
-  const oauth2 = google.oauth2({
-    auth: oauth2Client,
-    version: "v2"
-  })
-  const res = await oauth2.userinfo.get()
-  console.log(res.data)
-  */
+  if (tokens) {
+    (await cookies()).set('session', JSON.stringify(tokens), {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 60 * 60 * 24 * 7,
+      path: '/'
+    })
+  }
 
   redirect("/")
 }
