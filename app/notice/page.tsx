be1f96a1b468fe96e45e92e3d0b3fe5d33c43b42
LISPCoC <fourier0520@gmail.com>
2026-02-05 11:24:54 +0900

    update

--- app/notice/page.tsx
+++ app/notice/page.tsx
@@ -1,11 +1,26 @@
 
-import { } from "react"
+import Link from "next/link"
 
 export default function Chats() {
   return (
     <div className="w-full max-w-4xl ">
       <h2 className="text-xl font-bold pt-5 pb-5">お知らせ</h2>
       <div className="w-full pb-10">
+        <h3 className="font-bold pt-5 pb-5">チャット軽量化のトライアル運用について</h3>
+        <p>
+          利用者増加に伴い、チャットの動作が重くなり不安定化する現象が増えてきたため、軽量化のための回収トライアルを行います。
+        </p>
+        <p>
+          トライアルで大きな問題が出なければ正式に切り替える予定です。
+        </p>
+        <p>
+          不具合などを発見した場合は、<Link className="font-bold text-gray-700 hover:text-blue-700" href='/report' prefetch={false}>報告フォーム</Link>よりご報告お願いします。
+        </p>
+        <p>
+          実施予定期間：<span className="font-bold">2026/02/06 8:00 ～ 2026/02/07 8:00</span>頃
+        </p>
+
+        <h3 className="font-bold pt-5 pb-5">ルームの認証必須化について</h3>
         <p>
           ルーム作成時の認証必須化に伴って、過去に作成されて認証がされていないルームの稼働を停止します。
         </p>
--- components/headLine.tsx
+++ components/headLine.tsx
@@ -4,8 +4,13 @@ import styles from '@/components/style';
 export default function HeadLine() {
 
   return (
-    <Link className={`${styles.link} text-xs`} href="/notice" prefetch={false}>
-      お知らせ:ルームの認証必須化について
-    </Link>
+    <>
+      <Link className={`${styles.link} text-xs`} href="/notice" prefetch={false}>
+        お知らせ:チャット軽量化のトライアル運用について
+      </Link>
+      <Link className={`${styles.link} text-xs`} href="/notice" prefetch={false}>
+        お知らせ:ルームの認証必須化について
+      </Link>
+    </>
   )
 }
